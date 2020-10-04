3846814857f52fea30300e316d59a816766ec758
German Abramov <germanjke@gmail.com>
2020-10-04 21:25:34 +0300

    Update __init__.py

--- cigarette_butt_segmentation/lib/__init__.py
+++ cigarette_butt_segmentation/lib/__init__.py
@@ -3,3 +3,4 @@ from lib.utils import encode_rle, decode_rle, get_mask, download_masks
 from lib.show import show_img_with_mask, visualize
 from lib.html import get_html
 from lib.dataset import Dataset
+from lib.albumentations import *
